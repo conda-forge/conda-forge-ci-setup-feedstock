b894c62d178c057b05f4adc39214e47fd40c818a
Isuru Fernando <isuruf@gmail.com>
2019-05-04 13:44:15 -0500

    Set CI variable in CONFIG.yaml

--- recipe/run_conda_forge_build_setup_win.bat
+++ recipe/run_conda_forge_build_setup_win.bat
@@ -12,7 +12,20 @@ conda.exe config --set add_pip_as_python_dependency false
 call setup_x64
 
 :: Set the conda-build working directory to a smaller path
-set "CONDA_BLD_PATH=C:\\bld\\"
+if "%CONDA_BLD_PATH%"== "" (
+    set "CONDA_BLD_PATH=C:\\bld\\"
+)
+
+echo >> .ci_support/%CONFIG%.yaml
+echo CI: >> .ci_support/%CONFIG%.yaml
+echo - %CI% .ci_support/%CONFIG%.yaml
+echo >> .ci_support/%CONFIG%.yaml
+
+cat .ci_support/%CONFIG%.yaml
+
+echo set "CONDA_BLD_PATH=%CONDA_BLD_PATH%"         > "%CONDA_PREFIX%\etc\conda\activate.d\conda-forge-ci-setup-activate.bat"
+echo set "CPU_COUNT=%CPU_COUNT%"                  >> "%CONDA_PREFIX%\etc\conda\activate.d\conda-forge-ci-setup-activate.bat"
+echo set "PYTHONUNBUFFERED=%PYTHONUNBUFFERED%"    >> "%CONDA_PREFIX%\etc\conda\activate.d\conda-forge-ci-setup-activate.bat"
 
 conda.exe info
 conda.exe config --show-sources
