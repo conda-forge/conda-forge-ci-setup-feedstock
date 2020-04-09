fb76d9cac73f4a153e0e0cd002f6aaddaa2dcca7
Isuru Fernando <isuruf@gmail.com>
2020-04-09 18:37:12 -0500

    use os.path.sep

--- recipe/conda_forge_ci_setup/upload_or_check_non_existence.py
+++ recipe/conda_forge_ci_setup/upload_or_check_non_existence.py
@@ -22,7 +22,7 @@ def split_pkg(pkg):
     if not pkg.endswith(".tar.bz2"):
         raise RuntimeError("Can only process packages that end in .tar.bz2")
     pkg = pkg[:-8]
-    plat, pkg_name = pkg.split("/")
+    plat, pkg_name = pkg.split(os.path.sep)
     name_ver, build = pkg_name.rsplit('-', 1)
     name, ver = name_ver.rsplit('-', 1)
     return plat, name, ver, build
