96413ad27942c6d66600538c43f310c865a27c17
Isuru Fernando <isuruf@gmail.com>
2020-04-10 10:59:35 -0500

    Just move

--- recipe/conda_forge_ci_setup/mangle_homebrew.py
+++ recipe/conda_forge_ci_setup/mangle_homebrew.py
@@ -3,25 +3,6 @@ import os
 import shutil
 import sys
 
-
-def _try_move_file_or_dir(p, mangled_p):
-    try:
-        shutil.move(p, mangled_p)
-        print("MOVED FILE/DIR %s -> %s" % (p, mangled_p))
-    except shutil.Error:
-        try:
-            os.remove(p)
-            print("REMOVED FILE %s " % p)
-        except Exception as e:
-            print("ERROR moving or removing FILE %s: %s" % (p, repr(e)))
-            try:
-                shutil.rmtree(p, ignore_errors=True)
-                print("REMOVED DIR %s " % p)
-            except Exception as e:
-                print("ERROR moving or removing DIR %s: %s" % (p, repr(e)))
-    sys.stdout.flush()
-
-
 def main():
     # make the mangled path
     mangled_dir = "/usr/local/conda_mangled"
@@ -39,8 +20,7 @@ def main():
         if _pth in excluded_dirs:
             continue
         mangled_pth = os.path.join(mangled_dir, _pth)
-        _try_move_file_or_dir(pth, mangled_pth)
-
+        shutil.move(p, mangled_pth)
 
 if __name__ == "__main__":
     main()
