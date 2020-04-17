694ce6336681c544cb94be6802abe189788607b3
Matthew R. Becker <becker.mr@gmail.com>
2020-04-17 09:37:25 -0500

    ENH make it more installable

--- recipe/conda_forge_ci_setup/__init__.py
+++ recipe/conda_forge_ci_setup/__init__.py
@@ -1,2 +1,1 @@
 """conda-forge-ci-utils"""
-__version__ = '2.0.6'
--- recipe/setup.py
+++ recipe/setup.py
@@ -1,15 +1,35 @@
+import os
 from setuptools import setup, find_packages
 
-packages = find_packages()
+__version__ = "0.0.1"
+
+if "RECIPE_DIR" in os.environ:
+    pth = os.path.join(os.environ["RECIPE_DIR"], "meta.yaml")
+else:
+    pth = os.path.join(os.path.dirname(__file__), "meta.yaml")
+
+if os.path.exists(pth):
+    with open(pth, "r") as fp:
+        for line in fp.readlines():
+            if line.startswith("{% set version"):
+                __version__ = eval(
+                    line
+                    .strip()
+                    .split("=")[1]
+                    .strip()
+                    .replace("%}", "")
+                    .strip()
+                )
+                break
 
 setup(
     name="conda_forge_ci_setup",
-    version="2.5.0",
+    version=__version__,
     description="conda-forge-ci-utils",
     author="conda-forge/core",
     author_email="conda-forge/core@github.com",
     url="https://conda-forge.org",
-    packages=packages,
+    packages=find_packages(),
     entry_points={
         "console_scripts": [
             "ff_ci_pr_build = conda_forge_ci_setup.ff_ci_pr_build:main",
