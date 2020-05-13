e516296f0fe8441747406894648ce04da2a48675
Matthew R Becker <beckermr@users.noreply.github.com>
2020-05-13 07:35:02 -0500

    ENH do validation locally (#92)
    * ENH do validation locally
    
    * turn on validatoon for testing
    
    * MNT: Re-rendered with conda-build 3.19.2, conda-smithy 3.7.1, and conda-forge-pinning 2020.05.12.18.03.58
    
    * fix import
    
    * BUG proper method for dict values
    
    * DOC add a message
    
    * try a path source
    
    * try this
    
    * well that was dumb
    
    * BUG lower-case us
    
    * try again
    
    * try this
    
    * try this copy
    
    * try this
    
    * MNT: Re-rendered with conda-build 3.19.2, conda-smithy 3.7.1, and conda-forge-pinning 2020.05.12.18.03.58
    
    * grr
    
    * MNT: Re-rendered with conda-build 3.19.2, conda-smithy 3.7.1, and conda-forge-pinning 2020.05.12.18.03.58
    
    * Update feedstock_outputs.py
    
    * paths
    
    * grr windows
    
    * hack around azure windows
    
    Co-authored-by: conda-forge-linter <github-actions@email.com>

--- recipe/conda_forge_ci_setup/feedstock_outputs.py
+++ recipe/conda_forge_ci_setup/feedstock_outputs.py
@@ -2,7 +2,9 @@ import os
 import sys
 import hashlib
 import json
-import pprint
+import tempfile
+import subprocess
+import shutil
 
 import conda_build
 import conda_build.config
@@ -11,6 +13,17 @@ import click
 
 VALIDATION_ENDPOINT = "https://conda-forge.herokuapp.com"
 STAGING = "cf-staging"
+OUTPUTS_REPO = "https://github.com/conda-forge/feedstock-outputs.git"
+
+
+def split_pkg(pkg):
+    if not pkg.endswith(".tar.bz2"):
+        raise RuntimeError("Can only process packages that end in .tar.bz2")
+    pkg = pkg[:-8]
+    plat, pkg_name = pkg.split(os.path.sep)
+    name_ver, build = pkg_name.rsplit('-', 1)
+    name, ver = name_ver.rsplit('-', 1)
+    return plat, name, ver, build
 
 
 def _unix_dist_path(path):
@@ -71,6 +84,70 @@ def request_copy(feedstock, dists, channel, git_sha=None):
     return r.status_code == 200
 
 
+def is_valid_feedstock_output(project, outputs):
+    """Test if feedstock outputs are valid (i.e., the outputs are allowed for that
+    feedstock). Optionally register them if they do not exist.
+
+    Parameters
+    ----------
+    project : str
+        The GitHub repo.
+    outputs : list of str
+        A list of ouputs top validate. The list entries should be the
+        full names with the platform directory, version/build info, and file extension
+        (e.g., `noarch/blah-fa31b0-2020.04.13.15.54.07-py_0.tar.bz2`).
+
+    Returns
+    -------
+    valid : dict
+        A dict keyed on output name with True if it is valid and False
+        otherwise.
+    """
+    if project.endswith("-feedstock"):
+        feedstock = project[:-len("-feedstock")]
+    else:
+        feedstock = project
+
+    valid = {o: False for o in outputs}
+
+    tmpdir = None
+    try:
+        tmpdir = tempfile.mkdtemp('_recipe')
+        repo_path = os.path.join(tmpdir, "feedstock-outputs")
+
+        subprocess.run(
+            ["git", "clone", "--depth=1", OUTPUTS_REPO, repo_path],
+            check=True,
+        )
+
+        for dist in outputs:
+            try:
+                _, o, _, _ = split_pkg(dist)
+            except RuntimeError:
+                continue
+
+            pth = os.path.join(repo_path, "outputs", o + ".json")
+
+            if not os.path.exists(pth):
+                # no output exists and we can add it
+                valid[dist] = True
+            else:
+                # make sure feedstock is ok
+                with open(pth, "r") as fp:
+                    data = json.load(fp)
+                valid[dist] = feedstock in data["feedstocks"]
+    finally:
+        if tmpdir is not None:
+            # windows builds on azure sometimes fail when trying to remove
+            # tmpdirs, so we try and if it fails just move on
+            try:
+                shutil.rmtree(tmpdir)
+            except Exception:
+                pass
+
+    return valid
+
+
 @click.command()
 @click.argument("feedstock_name", type=str)
 def main(feedstock_name):
@@ -85,38 +162,12 @@ def main(feedstock_name):
             os.path.join(conda_build.config.subdir, p)
             for p in os.listdir(os.path.join(conda_build.config.croot, conda_build.config.subdir))  # noqa
         ])
-    built_distributions = [
-        _unix_dist_path(path) for path in paths if path.endswith('.tar.bz2')
-    ]
+    built_distributions = [path for path in paths if path.endswith('.tar.bz2')]
 
-    print("validating outputs:\n%s" % pprint.pformat(built_distributions))
-
-    r = requests.post(
-        "%s/feedstock-outputs/validate" % VALIDATION_ENDPOINT,
-        json={
-            "feedstock": feedstock_name,
-            "outputs": built_distributions,
-        },
-    )
-
-    if r.status_code != 200:
-        print(
-            "ERROR: output validation failed - your recipe/feedstock is "
-            "producing outputs that are already used by another "
-            "recipe/feedstock!"
-        )
-
-    try:
-        results = r.json()
-    except Exception as e:
-        print(
-            "ERROR getting output validation information "
-            "from the webservice:",
-            repr(e)
-        )
-        results = {}
+    results = is_valid_feedstock_output(feedstock_name, built_distributions)
 
     print("validation results:\n%s" % json.dumps(results, indent=2))
+    print("NOTE: Any outputs marked as False are not allowed for this feedstock.")
 
-    if r.status_code != 200:
+    if not all(v for v in results.values()):
         sys.exit(1)
