7a06a309328dfb65ec9c9fd986b2e308421da03f
Matthew R Becker <beckermr@users.noreply.github.com>
2020-07-28 09:49:12 -0500

    Update cross_compile_support.sh

--- recipe/cross_compile_support.sh
+++ recipe/cross_compile_support.sh
@@ -1,16 +1,5 @@
 BUILD_PLATFORM=$(conda info --json | jq -r .platform)
 
-if [[ ${BUILD_PLATFORM} == "linux-64" ]]; then
-  export BUILD="x86_64-conda-linux-gnu"
-  echo "export BUILD=\"${BUILD}\"" >> "${CONDA_PREFIX}/etc/conda/activate.d/conda-forge-ci-setup-activate.sh"
-elif [[ ${BUILD_PLATFORM} == "linux-ppc64le" ]]; then
-  export BUILD="powerpc64le-conda-linux-gnu"
-  echo "export BUILD=\"${BUILD}\"" >> "${CONDA_PREFIX}/etc/conda/activate.d/conda-forge-ci-setup-activate.sh"
-elif [[ ${BUILD_PLATFORM} == "linux-aarch64" ]]; then
-  export BUILD="aarch64-conda-linux-gnu"
-  echo "export BUILD=\"${BUILD}\"" >> "${CONDA_PREFIX}/etc/conda/activate.d/conda-forge-ci-setup-activate.sh"
-fi
-
 if [ -f ${CI_SUPPORT}/${CONFIG}.yaml ]; then
     HOST_PLATFORM=$(cat ${CI_SUPPORT}/${CONFIG}.yaml | shyaml get-value target_platform.0 ${BUILD_PLATFORM})
 fi
