862b898a86d5427a8251bb9605e63b1ae185ea95
Isuru Fernando <isuruf@gmail.com>
2021-01-18 20:14:58 -0600

    Fix QEMU_LD_PREFIX

--- recipe/cross_compile_support.sh
+++ recipe/cross_compile_support.sh
@@ -15,6 +15,6 @@ if [[ "${HOST_PLATFORM}" != "${BUILD_PLATFORM}" ]]; then
     fi
     if [[ "${BUILD_PLATFORM}" == "linux-64" && "${HOST_PLATFORM}" == linux-* ]]; then
         conda create -n sysroot_${HOST_PLATFORM} --yes --quiet sysroot_${HOST_PLATFORM}
-        export QEMU_LD_PREFIX=$CONDA_PREFIX/envs/sysroot_${HOST_PLATFORM}/*/sysroot
+        export QEMU_LD_PREFIX=$(find ${CONDA_PREFIX}/envs/sysroot_${HOST_PLATFORM} -name sysroot | head -1)
     fi
 fi
