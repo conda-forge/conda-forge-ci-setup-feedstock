7bf9f1bc4bb005667642c6423f2567a42f77281d
H. Vetinari <h.vetinari@gmx.com>
2021-08-01 17:30:25 +0200

    try invocation using -ArgumentList

--- recipe/run_conda_forge_build_setup_win.bat
+++ recipe/run_conda_forge_build_setup_win.bat
@@ -34,7 +34,7 @@ if "%CI%" == "azure" (
     )
     if "%CONDA_BLD_PATH%" == "D:\\bld\\" (
         echo CONDA_BLD_PATH=%CONDA_BLD_PATH%; Setting pagefile size to 8GB on C:
-        PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& '%EntryPointPath%' -MinimumSize 8GB -MaximumSize 8GB -DiskRoot """"C:"""" "
+        PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File """"%EntryPointPath%"""" -MinimumSize 8GB -MaximumSize 8GB -DiskRoot """""C:""""" '
     )
 )
 
