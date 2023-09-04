d8c861b01395387dc369c77fd2cc45fb103cf2c3
H. Vetinari <h.vetinari@gmx.com>
2023-09-04 18:16:21 +1100

    remove handling for WITH_LATEST_OSX_SDK, it has no users in conda-forge anymore

--- recipe/download_osx_sdk.sh
+++ recipe/download_osx_sdk.sh
@@ -7,11 +7,6 @@ export MACOSX_DEPLOYMENT_TARGET=${MACOSX_DEPLOYMENT_TARGET:-10.9}
 # Some project require a new SDK version even though they can target older versions
 if [ -f ${CI_SUPPORT}/${CONFIG}.yaml ]; then
     export MACOSX_SDK_VERSION=$(cat ${CI_SUPPORT}/${CONFIG}.yaml | shyaml get-value MACOSX_SDK_VERSION.0 0)
-    export WITH_LATEST_OSX_SDK=$(cat ${CI_SUPPORT}/${CONFIG}.yaml | shyaml get-value WITH_LATEST_OSX_SDK.0 0)
-    if [[ "${WITH_LATEST_OSX_SDK}" != "0" ]]; then
-        echo "Setting WITH_LATEST_OSX_SDK is removed. Use MACOSX_SDK_VERSION to specify an explicit version for the SDK."
-        export MACOSX_SDK_VERSION=10.15
-    fi
 fi
 
 if [[ "${MACOSX_SDK_VERSION:-0}" == "0" ]]; then
