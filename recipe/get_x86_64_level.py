765fdd171984574039474e4bf92f5c501a68035a
Isuru Fernando <isuruf@gmail.com>
2025-05-22 13:20:01 -0500

    revert accidental push

--- recipe/get_x86_64_level.py
+++
@@ -1,9 +0,0 @@
-import archspec.cpu
-h = archspec.cpu.detect.host()
-for i in range(4, 0, -1):
-  check = f"x86_64_v{i}" if i > 1 else "x86_64"
-  if h == check or check in h.ancestors:
-    print(i)
-    break
-else:
-  print(0)
