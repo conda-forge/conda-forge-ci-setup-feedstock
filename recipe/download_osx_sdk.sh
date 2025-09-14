3c4d53901605deb3f410b871afd2515a7d457109
H. Vetinari <h.vetinari@gmx.com>
2025-09-14 15:50:21 +0200

    simplfy: no pushd/popd

--- recipe/download_osx_sdk.sh
+++ recipe/download_osx_sdk.sh
@@ -58,16 +58,14 @@ if [[ "1" == "1" ]]; then
         esac)
     echo "${sdk_sha256} *MacOSX${MACOSX_SDK_VERSION}.sdk.tar.xz" | shasum -a 256 -c
     mkdir -p "$(dirname "$CONDA_BUILD_SYSROOT")"
-    ls -ll $(dirname "$CONDA_BUILD_SYSROOT")
+    # delete symlink that may exist already, e.g. MacOSX15.5.sdk -> MacOSX.sdk
     rm -rf $CONDA_BUILD_SYSROOT
     if [[ "${MACOSX_SDK_VERSION:-0}" == "15.5" ]]; then
         tar -xf MacOSX${MACOSX_SDK_VERSION}.sdk.tar.xz
         # we've downloaded the whole https://github.com/alexey-lysiuk/macos-sdk repo, go into the right folder;
         # github names the folder in the tarball based on the hash of the commit we're using; rename for ease of use
         mv macos-sdk-* repo
-        pushd repo
-        mv MacOSX${MACOSX_SDK_VERSION}.sdk "$(dirname "$CONDA_BUILD_SYSROOT")/"
-        popd
+        mv repo/MacOSX${MACOSX_SDK_VERSION}.sdk "$(dirname "$CONDA_BUILD_SYSROOT")/"
     else
         tar -xf MacOSX${MACOSX_SDK_VERSION}.sdk.tar.xz -C "$(dirname "$CONDA_BUILD_SYSROOT")"
     fi
